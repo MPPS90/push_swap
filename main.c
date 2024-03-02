b9abd572e047f610b190f677a170256dd1ef42be
Maria Del Pilar Pena Soto <mpena-so@c1r16s6.42madrid.com>
2024-03-02 21:01:52 +0100

    comandos no es de push_swap

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 //main argumentos 
+
 int main(int argc, char *argv[])
 {
     int i;
@@ -14,3 +15,25 @@ int main(int argc, char *argv[])
         i++;
     }
 }
+
+/* Seleccionas con shift
+
+Coomand + -> = ir al final de la linea
+Coomand + <- = ir al principio de la linea
+Coomand + ^(flechita de arriba) = ir al principio del archivo
+Coomand +  (flechita de abajo) = ir al final del archivo
+ */
+
+// Mover lineas:
+//     donde sea que esté el cursor, presionar alt y flechitas abajo y arriba
+
+// Para duplicar:
+//     1.- Seleccionas
+//     2.- alt + shift + flecha arriba o flecha abajo
+
+// Comentario por bloques -->> selecciona lo que quieres comentar y luego
+//     alt + shift + a
+
+/* Comentario por lineas -->> selecciona lo que quieres comentar y luego
+    Command + shift + 7
+ */
