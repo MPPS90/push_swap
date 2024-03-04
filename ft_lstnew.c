33b2f037700dba2e7cdf170527bf967919869f5b
Maria Del Pilar Pena Soto <mpena-so@c2r19s6.42madrid.com>
2024-03-04 20:53:58 +0100

    ft_lstnew.c organizado

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,34 +6,18 @@
 /*   By: mpena-so <mpena-so@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/04 17:22:21 by mpena-so          #+#    #+#             */
-/*   Updated: 2024/03/04 20:49:10 by mpena-so         ###   ########.fr       */
+/*   Updated: 2024/03/04 20:53:18 by mpena-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <stdio.h>
-
 typedef struct s_list
 {
     void    *content;
     struct s_list *next;
 } t_list; 
 
-//Para poder ver lo que devuelve facilmente
-/* typedef struct s_list
-{
-    int content;
-    struct s_list *next;
-} t_list;  */ 
-
-/* 
-Estructura: 
-Tiene los datos
-Nodo: 
-Es una estructura que tiene dirección de memoria
-Lista:
-Conectar los nodos
-*/
 t_list *ft_lstnew(int content)
 {
     t_list *new_ls;
@@ -57,14 +41,18 @@ int main ()
     return 0;
 }
 
-
 //Para poder ver lo que devuelve facilmente
-/* int main ()
+/* typedef struct s_list
 {
-    t_list *alumno;
-    
-    int edad = 10;
-    alumno = ft_lstnew(edad);
-    printf("%d", alumno->content);
-    return 0;
-} */
+    int content; // se pone aquí int porque con el void* que indica en el subject no se podría ver el valor devuelto
+    struct s_list *next;
+} t_list;  */ 
+
+/* 
+Estructura: 
+Tiene los datos
+Nodo: 
+Es una estructura que tiene dirección de memoria
+Lista:
+Conectar los nodos
+*/
