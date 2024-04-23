1d0f78a1b698dfd8f763aed904b27c8146ffed4b
Maria Del Pilar Pena Soto <mpena-so@c2r19s5.42madrid.com>
2024-04-23 18:45:37 +0200

    fuera libft

--- rotate.c
+++ rotate.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   rotate.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mpena-so <mpena-so@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: mpena-so <mpena-so@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 22:40:37 by mpena-so          #+#    #+#             */
-/*   Updated: 2024/04/22 23:29:43 by mpena-so         ###   ########.fr       */
+/*   Updated: 2024/04/23 18:35:09 by mpena-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,21 @@ void    rotate(t_numbers **stack_a)
 {
     t_numbers   *first_node;
     t_numbers   *second_node;
-    t_numbers   *final_node;
+    //t_numbers   *final_node;
 
     first_node = *stack_a; //first_node guarda el puntero al primer nodo del stack a
     second_node = first_node->next;
-    final_node = second_node->next;
-    final_node->next = NULL; //final_node apunta a NULL (indica final de la lista)
+    //final_node = second_node->next;
+    //final_node->next = NULL; //final_node apunta a NULL (indica final de la lista)
+    *stack_a = second_node;
+    first_node->next = second_node;
 
-    while (first_node != NULL)
+/*     while (first_node != NULL)
     {
-        //*stack_a = final_node;
+        *stack_a = final_node;
         first_node = final_node->next;
         second_node = first_node->next;
         final_node = second_node->next;
         *stack_a = final_node;        
-    }
+    } */
 }
